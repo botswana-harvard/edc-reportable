80cd1c87f1a3793fb473bd3ba9089c1fc921b178
erikvw <ew2789@gmail.com>
2017-12-12 23:01:42 +0200

    add constants for display units

--- edc_reportable/__init__.py
+++ edc_reportable/__init__.py
@@ -7,4 +7,6 @@ from .site_reportables import site_reportables
 from .units import CELLS_PER_MILLIMETER_CUBED, COPIES_PER_MILLILITER
 from .units import IU_LITER, GRAMS_PER_DECILITER, TEN_X_9_PER_LITER, TEN_X_3_PER_LITER
 from .units import MILLIGRAMS_PER_DECILITER, MILLIMOLES_PER_LITER, MICROMOLES_PER_LITER
+from .units import CELLS_PER_MILLIMETER_CUBED_DISPLAY, TEN_X_3_PER_LITER_DISPLAY
+from .units import TEN_X_9_PER_LITER_DISPLAY
 from .value_reference_group import ValueReferenceGroup, NotEvaluated
--- edc_reportable/units.py
+++ edc_reportable/units.py
@@ -1,4 +1,7 @@
+from django.utils.safestring import mark_safe
+
 CELLS_PER_MILLIMETER_CUBED = 'cells/mm^3'
+CELLS_PER_MILLIMETER_CUBED_DISPLAY = mark_safe('cells/mm<sup>3</sup>')
 COPIES_PER_MILLILITER = 'copies/mL'
 GRAMS_PER_DECILITER = 'g/dL'
 IU_LITER = 'IU/L'
@@ -6,4 +9,6 @@ MICROMOLES_PER_LITER = 'umol/L'
 MILLIGRAMS_PER_DECILITER = 'mg/dL'
 MILLIMOLES_PER_LITER = 'mmol/L'
 TEN_X_3_PER_LITER = '10^3/L'
+TEN_X_3_PER_LITER_DISPLAY = mark_safe('10sup>3</sup>/L')
 TEN_X_9_PER_LITER = '10^9/L'
+TEN_X_9_PER_LITER_DISPLAY = mark_safe('10sup>9</sup>/L')
