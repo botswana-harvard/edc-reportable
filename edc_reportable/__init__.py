e1c9b0ab062e8bea03b61b3590996fe06d4ef93d
erikvw <ew2789@gmail.com>
2017-12-13 00:01:44 +0200

    add constants for units

--- edc_reportable/__init__.py
+++ edc_reportable/__init__.py
@@ -4,9 +4,9 @@ from .grade_reference import GradeReference, GRADE1, GRADE2, GRADE3, GRADE4, GRA
 from .normal_reference import NormalReference
 from .parsers import parse, unparse, ParserError
 from .site_reportables import site_reportables
-from .units import CELLS_PER_MILLIMETER_CUBED, COPIES_PER_MILLILITER
+from .units import CELLS_PER_MILLIMETER_CUBED, COPIES_PER_MILLILITER, MM3, MM3_DISPLAY
 from .units import IU_LITER, GRAMS_PER_DECILITER, TEN_X_9_PER_LITER, TEN_X_3_PER_LITER
 from .units import MILLIGRAMS_PER_DECILITER, MILLIMOLES_PER_LITER, MICROMOLES_PER_LITER
 from .units import CELLS_PER_MILLIMETER_CUBED_DISPLAY, TEN_X_3_PER_LITER_DISPLAY
-from .units import TEN_X_9_PER_LITER_DISPLAY
+from .units import TEN_X_9_PER_LITER_DISPLAY, MICROMOLES_PER_LITER_DISPLAY
 from .value_reference_group import ValueReferenceGroup, NotEvaluated
