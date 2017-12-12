83ce56abe4a856bccc389641c3491e4cb6c7f70f
erikvw <ew2789@gmail.com>
2017-12-12 06:41:31 +0200

    add units

--- edc_reportable/__init__.py
+++ edc_reportable/__init__.py
@@ -4,7 +4,7 @@ from .grade_reference import GradeReference, GRADE1, GRADE2, GRADE3, GRADE4, GRA
 from .normal_reference import NormalReference
 from .parsers import parse, unparse, ParserError
 from .site_reportables import site_reportables
-from .units import CELLS_PER_MILLIMETER_CUBED
+from .units import CELLS_PER_MILLIMETER_CUBED, COPIES_PER_MILLILITER
 from .units import IU_LITER, GRAMS_PER_DECILITER, TEN_X_9_PER_LITER, TEN_X_3_PER_LITER
 from .units import MILLIGRAMS_PER_DECILITER, MILLIMOLES_PER_LITER, MICROMOLES_PER_LITER
 from .value_reference_group import ValueReferenceGroup, NotEvaluated
